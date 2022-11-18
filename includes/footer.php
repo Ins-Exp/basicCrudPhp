8ce478b063adabedc150dcbdf4c7d9cd0442ee2c
birukbelay <belay.bura@gmail.com>
2022-11-18 11:45:57 +0300

    fixed some stuff

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="flex justify-between items-center bg-gray-200 text-gray-900 p-10 px-20">
+    <h1 class="text-2xl font-bold">Best Furnitures </h1>
+    <div class="flex px-10">
+        <h1>&copy; 2022 All right is reserved.</h1>
+    </div>
+</footer>
+
+</body>
+
+</html>
